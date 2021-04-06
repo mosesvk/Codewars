2bbfbc812ecb1438943359ce96e7b8878bf06043
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-06 10:06:57 -0600

    update 8kyu OPPOSITE folder

---
+++ 8kyu/Opposite/answer.js
@@ -0,0 +1,24 @@
+function opposite(number) {
+  //your code here
+  return number * -1
+}
+
+
+//Additional Questions 
+
+/// HTML/CSS: 
+// What is a pseudo-selector in CSS?
+// => Used to define a special state of an element, LVHA are the top ones (:link, :visited, :hover, :active)
+
+/// JavaScript: 
+// Describe what closures are, and why they are important?
+// => a function within a function. either belongs to local or global scope. 
+//    a global variable can be made local(private) with closures
+
+// What is Lexical Scoping? 
+// => Where Javascript starts at the innermost scope and searches OUTwards to find the variable. 
+//    Lexical scoping is nice, because we can easily figure out what the value of a variable will be by looking at the code; whereas in dynamic scoping, the meaning of a variable can change at runtime, making it more difficult.
+
+/// React: 
+// What is the difference between an element and a component?
+// => A component is what stores and renders and element (which is basically an object that is being rendered by the component)
---
+++ 8kyu/Opposite/instructions.js
@@ -0,0 +1,39 @@
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+function opposite(number) {
+  //your code here
+}
+
+//Additional Questions 
+
+// HTML/CSS: 
+// What is a pseudo-selector in CSS?
+
+// JavaScript: 
+// Describe what closures are, and why they are important. 
+
+// React: 
+// What is the difference between an element and a component?
+
+
+
+
+
+//Look Below for hints 
+
+//step 1: ask clarifying questions
+//step 2: function signature -> function(number) => number
+//step 3: sample data, what the function will take as an argument, and what it is expected to return with that argument
+//step 4: pseudo code (step by step of what you will be doing)
+//  write a function called opposite that takes a number parameter
+//  multiply that parameter by -1
+//  return the new number
+
+// Solve Problem
