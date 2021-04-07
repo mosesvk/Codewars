95d17b1881b60dc15c5f6ab7fa0998d15a2ebbd7
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-06 18:33:23 -0600

    please work

---
+++ 8kyu/Even_or_Odd/Instructions.js
@@ -0,0 +1,18 @@
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+  // ...
+}
+
+
+/// Additional Q's
+
+// HTML/CSS: 
+// Describe the box-model in HTML/CSS.
+// => 
+
+// JavaScript: 
+// What does hoisting refer to, and how does it work in JavaScript?
+
+// React: 
+// What is setState and what does it do?
