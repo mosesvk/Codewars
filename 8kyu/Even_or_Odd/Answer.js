cf743b4d9ec28778806f7166d20502ec277cb201
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-06 18:54:32 -0600

    Im the boss... psych

--- 8kyu/Even_or_Odd/Answer.js
+++ 8kyu/Even_or_Odd/Answer.js
@@ -5,21 +5,5 @@ function even_or_odd(number) {
   return number % 2 === 0 ? 'Even' : 'Odd' 
 }
 
-/// Additional Q's
 
-// HTML/CSS: 
-// Describe the box-model in HTML/CSS.
-// => essentially a box will always wrap around every HTML element. 
-//    that box consists of margins, borders, padding, and the content element itself.
-
-// JavaScript: 
-// What does hoisting refer to, and how does it work in JavaScript?
-// ==> this is JS's behavior of having DECLARATIONS in the top
-//     Remember to ALWAYS declare all variables at the top of the scope 
-//     This is good practice as it will help avoid any bugs.
-
-// React: 
-// What is setState and what does it do?
-//  ==> adds changes to the component's state and tells React that this component 
-//      and its children need to be re-rendered with the updated state.
 
--- 8kyu/Even_or_Odd/Instructions.js
+++ 8kyu/Even_or_Odd/Instructions.js
@@ -4,13 +4,3 @@ function even_or_odd(number) {
   // ...
 }
 
-/// Additional Q's
-
-// HTML/CSS: 
-// Describe the box-model in HTML/CSS.
-
-// JavaScript: 
-// What does hoisting refer to, and how does it work in JavaScript?
-
-// React: 
-// What is setState and what does it do?
