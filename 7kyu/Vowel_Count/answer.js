31afdf947b3247ced92ddb456584be28e1b232a0
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-07 21:05:49 -0600

    update vowel count Kata

---
+++ 7kyu/Vowel_Count/answer.js
@@ -0,0 +1,20 @@
+
+//answer (most clever by someone else) 
+function getCount(str) {
+  return (str.match(/[aeiou]/ig)||[]).length;
+}
+
+// answer 2
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = ["a","e","i","o","u"];
+  for(var i = 0;i < str.length;i++){
+    for(var j = 0; j < vowels.length; j++){
+      if(str[i] === vowels[j]){
+        vowelsCount++;
+      }
+    }
+  }
+  
+  return vowelsCount;
+}
