d0169c7601426843d16e93c3828e7f0e8cc37067
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-13 12:45:00 -0600

    update

--- 7kyu/Bubblesort_algorithm/instructions.js
+++ 7kyu/Bubblesort_algorithm/instructions.js
@@ -7,5 +7,8 @@
 // You then proceed by comparing the 2nd and 3rd terms, performing a swap when necessary, and then the 3rd and 4th term, then the 4th and 5th term, etc. etc. When you reach the end of the list, it is said that you have completed 1 complete pass.
 
 // Task
-// Given an array of integers, your function bubblesortOnce/bubblesort_once/BubblesortOnce (or equivalent, depending on your language's naming conventions) should return a new array equivalent to performing exactly 1 complete pass on the original array. Your function should be pure, i.e. it should not mutate the input array.
+// Given an array of integers, your function bubblesortOnce  should return a new array equivalent to performing exactly 1 complete pass on the original array. Your function should be pure, i.e. it should not mutate the input array.
 
+function bubblesortOnce(a) {
+  // TODO
+}
