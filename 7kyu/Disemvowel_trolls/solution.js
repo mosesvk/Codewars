f14df449ecd0d7437aa08a8839aa7f327ff223e4
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-13 10:24:34 -0600

    update

---
+++ 7kyu/Disemvowel_trolls/solution.js
@@ -0,0 +1,13 @@
+
+
+const disemvowel = string => string.replace(/[aeiou]/gi, '')
+//Remember the 'gi' is a modifier to do a case INSENSITIVE search because there are both capitalize and non cap letters. 
+
+// HTML/CSS: 
+// How could you make a star using CSS shapes only?
+
+// JavaScript: 
+// Describe how scoping works in JavaScript.
+
+// React: 
+// Where would you make AJAX or Axios requests in a React application?
