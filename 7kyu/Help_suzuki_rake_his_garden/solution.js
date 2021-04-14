6cfe5dca21a9bba6310fc365f0eb5abbaa0250a5
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-14 10:04:04 -0600

    udpate

---
+++ 7kyu/Help_suzuki_rake_his_garden/solution.js
@@ -0,0 +1,10 @@
+// Step 1: ask clarifying questions
+// Step 2: Function Signature : takes in a string and returns a string
+// Step 3: Sample Data: garden = 'slug spider rock gravel gravel gravel gravel gravel gravel gravel'
+// Step 4: pseudo code 
+
+function rakeGarden(garden) {
+  var rakedGarden;
+  // TODO: Program Me
+  return rakedGarden;
+}
