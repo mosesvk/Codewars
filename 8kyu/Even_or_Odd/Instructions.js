99bde00417fb6f4d1d62a8f4ae8fb3919c9c3f4a
Moses Kaumatule <kaumatule93@gmail.com>
2021-05-15 22:09:54 -0600

    update

--- 8kyu/Even_or_Odd/Instructions.js
+++ 8kyu/Even_or_Odd/Instructions.js
@@ -1,5 +1,9 @@
 // Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
+// So just to make sure that we only want numbers either even or odd? 
+// Are there any constraints? 
+// Will there only be numbers that are inputted? 
+
 function even_or_odd(number) {
   // ...
 
