6c0163ab778fd68057228c7bdfeca76dbd68d5fd
mosesvk <kaumatule93@gmail.com>
2021-06-14 09:54:47 -0600

    update

---
+++ 6kyu/Find_the_odd_int/instructions.md
@@ -0,0 +1,9 @@
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be ONLY ONE INTEGER that appears an odd number of times.
+
+Ex.
+[2, 2, 3, 3, 4, 4, 4]
+--> 
+return 4
+
---
+++ 6kyu/Find_the_odd_int/solution.js
@@ -0,0 +1,47 @@
+function findOdd(A) {
+  for (let i = 0; i <= A.length-1; i += 1) {
+  
+  let elmLength = A.filter((elm) => elm === A[i]).length;
+  
+  if (elmLength % 2 !== 0) {
+    return A[i];
+  }
+}
+}
+
+/////////////////////////////////////////////
+
+const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+
+/////////////////////////////////////////////
+
+function findOdd(A) {
+  var obj = {};
+  A.forEach(function(el){
+    obj[el] ? obj[el]++ : obj[el] = 1;
+  });
+  
+  for(prop in obj) {
+    if(obj[prop] % 2 !== 0) return Number(prop);
+  }
+}
+
+/////////////////////////////////////////////
+
+const findOdd = (arr) => {
+  let odd = {};
+
+  for (let i = 0; i < arr.length; i++){
+    if (odd[arr[i]]){
+      odd[arr[i]]++;
+    } else {
+      odd[arr[i]] = 1;
+    }
+  }
+
+  for (let j in odd){
+    if (odd[j] % 2 !== 0){
+      return Number(j)
+    }
+  }
+}
