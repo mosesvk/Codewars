2b9ac263f06236a16155ecc621f2eaa5a9a42a8b
mosesvk <kaumatule93@gmail.com>
2021-06-22 22:59:38 -0600

    update

--- 6kyu/Find_the_odd_int/solution.js
+++ 6kyu/Find_the_odd_int/solution.js
@@ -1,13 +1,4 @@
-function findOdd(A) {
-  for (let i = 0; i <= A.length-1; i += 1) {
-  
-  let elmLength = A.filter((elm) => elm === A[i]).length;
-  
-  if (elmLength % 2 !== 0) {
-    return A[i];
-  }
-}
-}
+
 
 /////////////////////////////////////////////
 
