48809de57defc3b835a6020da63f12a647cee6f2
mosesvk <kaumatule93@gmail.com>
2022-03-26 11:27:07 -0600

    customizing works section

--- 7kyu/Vowel_Count/solution.js
+++ 7kyu/Vowel_Count/solution.js
@@ -1,40 +1,58 @@
-
-//answer (most clever by someone else) 
+//answer (most clever by someone else)
 function getCount(str) {
-  return (str.match(/[aeiou]/ig) || []).length;
+  return (str.match(/[aeiou]/gi) || []).length;
   // It actually works without the || [] but we might need that in the future
+}
+
+//another quick answer --> updated 03-26-2022
+function getCount(str) {
+  var vowelsCount = 0;
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  str.split('').filter((item) => {
+    if (vowels.includes(item)) vowelsCount++;
+  });
+
+  return vowelsCount;
+}
+
+//shorter version of That
+function getCount(str) {
+  return str.split('').filter((c) => 'aeiouAEIOU'.includes(c)).length;
 }
 
 // answer 2
 function getCount(str) {
   var vowelsCount = 0;
-  var vowels = ["a","e","i","o","u"];
-  for(var i = 0;i < str.length;i++){
-    for(var j = 0; j < vowels.length; j++){
-      if(str[i] === vowels[j]){
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < vowels.length; j++) {
+      if (str[i] === vowels[j]) {
         vowelsCount++;
       }
     }
   }
-  
+
   return vowelsCount;
 }
 
-// Another way I figured 
+// Another way I figured
 function getCount(str) {
   var vowelsCount = 0;
-  
+
   // enter your majic here
-  var split = str.split('')
-  for (let i = 0; i < split.length; i++){
-    if (split[i] === 'a' || 
-        split[i] === 'e' || 
-        split[i] === 'i' ||
-        split[i] === 'o' || 
-        split[i] === 'u'){
-      vowelsCount++
+  var split = str.split('');
+  for (let i = 0; i < split.length; i++) {
+    if (
+      split[i] === 'a' ||
+      split[i] === 'e' ||
+      split[i] === 'i' ||
+      split[i] === 'o' ||
+      split[i] === 'u'
+    ) {
+      vowelsCount++;
     }
   }
-  
+
   return vowelsCount;
 }
