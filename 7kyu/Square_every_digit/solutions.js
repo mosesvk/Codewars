7b303afc9011500dcfdbf3f0f46db3bfb253f120
mosesvk <kaumatule93@gmail.com>
2022-03-28 13:51:55 -0600

    7ku

--- 7kyu/Square_every_digit/solutions.js
+++ 7kyu/Square_every_digit/solutions.js
@@ -1,19 +1,28 @@
 // my Solution
 
-function squareDigits(num){
+function squareDigits(num) {
+  let numArray = Array.from(num.toString()).map(Number);
 
-  let numArray = Array.from(num.toString()).map(Number)
-
-  return Number(numArray.map(item => Math.pow(item,     2)).join(''))
-  
+  return Number(numArray.map((item) => Math.pow(item, 2)).join(''));
 }
 
-// the quickest solutions 
+// the quickest solutions
 
-function squareDigits(num){
-  return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
+function squareDigits(num) {
+  return Number(
+    ('' + num)
+      .split('')
+      .map(function (val) {
+        return val * val;
+      })
+      .join('')
+  );
 }
 
-function squareDigits(num){
-  return +num.toString().split('').map(i => i*i).join('');
+function squareDigits(num) {
+  return +num
+    .toString()
+    .split('')
+    .map((i) => i * i)
+    .join('');
 }
