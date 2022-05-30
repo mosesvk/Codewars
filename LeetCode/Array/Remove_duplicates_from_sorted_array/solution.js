62136d1d14e782216bdce9d7cdc85eb5e8121399
mosesvk <kaumatule93@gmail.com>
2022-05-30 08:31:44 -0600

    leetcode practice

--- LeetCode/Array/Remove_duplicates_from_sorted_array/solution.js
+++ LeetCode/Array/Remove_duplicates_from_sorted_array/solution.js
@@ -1,32 +1,24 @@
-
-
 const removeDuplicates = nums => {
 
-  for (let i = 0; i < nums.length - 1; i++){
-    for (let j = 1; j < nums.length; j++){
-      if(nums[i] === nums[j]){
-        nums.splice(j, 1)
-      }
-    }
-  }
-  return nums
+
+
 }
 
 
 // var removeDuplicates = function(nums) {
-//   var len = nums.length;
-//   var last = null;
-//   var count = 0;
+  // var len = nums.length;
+  // var last = null;
+  // var count = 0;
 
-//   for (var i = 0; i < len; i++) {
-//     if (nums[i] !== last) {
-//       nums[count] = nums[i];
-//       last = nums[i];
-//       count++;
-//     }
-//   }
+  // for (var i = 0; i < len; i++) {
+  //   if (nums[i] !== last) {
+  //     nums[count] = nums[i];
+  //     last = nums[i];
+  //     count++;
+  //   }
+  // }
 
-//   return count;
+  // return count;
 // };
 
 
