f6bc63947a6812c97f4cf956842be73b5cb11155
mosesvk <kaumatule93@gmail.com>
2022-05-30 08:26:49 -0600

    leetcode practice

--- LeetCode/Array/Remove_duplicates_from_sorted_array/solution.js
+++ LeetCode/Array/Remove_duplicates_from_sorted_array/solution.js
@@ -1,6 +1,3 @@
-const removeDuplicates = nums => {
-
-}
 
 
 const removeDuplicates = nums => {
@@ -16,34 +13,34 @@ const removeDuplicates = nums => {
 }
 
 
-var removeDuplicates = function(nums) {
-  var len = nums.length;
-  var last = null;
-  var count = 0;
+// var removeDuplicates = function(nums) {
+//   var len = nums.length;
+//   var last = null;
+//   var count = 0;
 
-  for (var i = 0; i < len; i++) {
-    if (nums[i] !== last) {
-      nums[count] = nums[i];
-      last = nums[i];
-      count++;
-    }
-  }
+//   for (var i = 0; i < len; i++) {
+//     if (nums[i] !== last) {
+//       nums[count] = nums[i];
+//       last = nums[i];
+//       count++;
+//     }
+//   }
 
-  return count;
-};
+//   return count;
+// };
 
 
 ////////////////////////////////////////////////////////////
 
-var removeDuplicates = function(nums) {
-  let i = 0;
-  for(let j = 1; j < nums.length; j++){
-      if(nums[i] != nums[j]){
-          i++;
-          nums[i]=nums[j];
-      }
-  }
-  return i+1;
-};
+// var removeDuplicates = function(nums) {
+//   let i = 0;
+//   for(let j = 1; j < nums.length; j++){
+//       if(nums[i] != nums[j]){
+//           i++;
+//           nums[i]=nums[j];
+//       }
+//   }
+//   return i+1;
+// };
 
 ///////////////////////////////////////////////////////////
