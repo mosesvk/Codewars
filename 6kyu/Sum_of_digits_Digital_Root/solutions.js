a2cba9cf91018c23d44ff297623e56e25b54847c
mosesvk <kaumatule93@gmail.com>
2022-06-20 06:12:38 -0600

    practice

--- 6kyu/Sum_of_digits_Digital_Root/solutions.js
+++ 6kyu/Sum_of_digits_Digital_Root/solutions.js
@@ -1,21 +1,25 @@
 // Quickest Solution
 
 function digital_root(n) {
-  return (n - 1) % 9 + 1;
+  return ((n - 1) % 9) + 1;
 }
-
 
 // Other SOLUTIONS
 
 function digital_root(n) {
   if (n < 10) return n;
-  
+
   return digital_root(
-    n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+    n
+      .toString()
+      .split('')
+      .reduce(function (acc, d) {
+        return acc + +d;
+      }, 0)
+  );
 }
 
-
 function digital_root(n) {
-  if (n < 10) return n
-  return digital_root(n % 10 + digital_root(Math.floor(n / 10)))
+  if (n < 10) return n;
+  return digital_root((n % 10) + digital_root(Math.floor(n / 10)));
 }
